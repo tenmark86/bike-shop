5c78c5a46b5b009d40cd0d03f7eef9404df5c8d7
tenmark86e <tenmark86@gmail.com>
2022-10-05 12:09:06 +0300

    added style attribute

---
+++ src/components/CountPicker/styled.js
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Input = styled.input.attrs({
+    type: "text",
+})`
+    border: none;
+    outline: none;
+    font-size: 18px;
+    font-weight: 500;
+    text-align: center;
+    width: 20px;
+    color: #282828;
+`;
+
+export const Button = styled.button`
+    border: none;
+    background: none;
+    font-size: 18px;
+    font-weight: 500;
+    color: #B5B5B5;
+    cursor: pointer;
+`;
